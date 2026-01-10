6d22f91896cac64e22f61f0c437b9521acce1262
darsh609 <116168977+darsh609@users.noreply.github.com>
2026-01-11 00:37:22 +0530

    gittt

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -2,11 +2,11 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: 587,
   auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
-  }
+  },
+   secure: false,
 });
 
 module.exports = async (email, otp) => {
